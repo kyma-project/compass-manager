5ecc604b58c445bdef0f72c3be76037c99e5f46f
Rafał Foks <r.fok74@gmail.com>
2023-04-03 13:52:59 +0200

    Change namespace, domain name

--- main.go
+++ main.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2023 framefrog.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
@@ -67,7 +51,7 @@ func main() {
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "2647ec81.compass.kyma-project.io",
+		LeaderElectionID:       "2647ec81.compass-manager.kyma-project.io",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
